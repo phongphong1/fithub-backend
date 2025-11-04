d5dd5fb6ab4b87e1f2f1e4f40de9534f5346f2f4
fsa_PhongHV <5247-fsa_PhongHV@users.noreply.fa-git.fsoft.com.vn>
2025-11-04 21:57:53 +0700

    Add repositories

---
+++ src/main/java/fa/training/fithub/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package fa.training.fithub.repository;
+
+import fa.training.fithub.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
