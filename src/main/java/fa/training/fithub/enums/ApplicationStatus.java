af9dabfdbc75485aab8eab28ca093e0008ec205d
fsa_PhongHV <5247-fsa_PhongHV@users.noreply.fa-git.fsoft.com.vn>
2025-11-04 17:03:01 +0700

    Add entities

---
+++ src/main/java/fa/training/fithub/enums/ApplicationStatus.java
@@ -0,0 +1,7 @@
+package fa.training.fithub.enums;
+
+public enum ApplicationStatus {
+    pending,
+    approved,
+    rejected
+}
---
+++ src/main/java/fa/training/fithub/enums/TokenType.java
@@ -0,0 +1,8 @@
+package fa.training.fithub.enums;
+
+public enum TokenType {
+    refresh,
+    reset_password,
+    email_verification,
+    other
+}
